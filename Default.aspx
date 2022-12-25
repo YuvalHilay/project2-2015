0b907f7155c9c5484ee79dc5883127cf3f81eb8e
YuvalHilay <73775490+YuvalHilay@users.noreply.github.com>
2022-12-25 17:50:00 +0200

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="_29._4_htm_._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="lblMessage" runat="server"></asp:Label>
+    
+        <asp:Label ID="count" runat="server"></asp:Label>
+    
+    </div>
+    <p>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        <asp:Button ID="btnCount" runat="server" onclick="Button1_Click" 
+            style="margin-bottom: 0px; margin-top: 8px;" Text="בדוק" Width="654px" />
+    </p>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _29._4_htm_
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+       
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            Random r = new Random();
+            if(Page.IsPostBack == false)
+            ViewState["num"] = r.Next(10).ToString();
+            ViewState["count"] = "0";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(int.Parse(TextBox1.Text.ToString()) == int.Parse(ViewState["num"].ToString()))
+                lblMessage.Text ="you are the winner" + ViewState["num"].ToString();
+            else
+            int count = int.Parse(ViewState["count"].ToString());
+            count++;
+            ViewState["count"] = count;
+            
+            lblMessage.Text = "try again"+ count;
+        }
+    }
+}
